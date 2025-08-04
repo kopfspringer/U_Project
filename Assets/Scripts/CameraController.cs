272123327f15752e2b9e25a1b5177d153e5d1168
kopfspringer <74976028+kopfspringer@users.noreply.github.com>
2025-08-04 14:17:13 +0200

    Enable camera movement by mouse drag

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -12,6 +12,8 @@ public class CameraController : MonoBehaviour
 
     public float moveSpeed;
     private Vector3 moveTarget;
+    public float dragSpeed = 0.1f;
+    private Vector3 lastMousePosition;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -22,9 +24,29 @@ public class CameraController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        HandleMouseDrag();
+
         if (moveTarget != transform.position)
         {
             transform.position = Vector3.MoveTowards(transform.position, moveTarget, moveSpeed * Time.deltaTime);
+        }
+    }
+
+    private void HandleMouseDrag()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            lastMousePosition = Input.mousePosition;
+            moveTarget = transform.position;
+        }
+        else if (Input.GetMouseButton(0))
+        {
+            Vector3 delta = Input.mousePosition - lastMousePosition;
+            Vector3 move = new Vector3(-delta.x, 0f, -delta.y) * dragSpeed;
+            transform.position += move;
+
+            lastMousePosition = Input.mousePosition;
+            moveTarget = transform.position;
         }
     }
 
