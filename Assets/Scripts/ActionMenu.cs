326027f1166a8e88e3ea13013656599d95d7eda8
kopfspringer <74976028+kopfspringer@users.noreply.github.com>
2025-08-04 18:30:40 +0200

    Add action menu with attack option

---
+++ Assets/Scripts/ActionMenu.cs
@@ -0,0 +1,84 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ActionMenu : MonoBehaviour
+{
+    public static ActionMenu instance;
+
+    [Header("UI References")]
+    public Button actionButton;
+    public GameObject dropUpPanel;
+    public Button attackButton;
+    public Button magicButton;
+    public Button restButton;
+
+    private void Awake()
+    {
+        instance = this;
+    }
+
+    void Start()
+    {
+        HideMenu();
+
+        if (actionButton != null)
+        {
+            actionButton.onClick.AddListener(ToggleDropUp);
+        }
+
+        if (attackButton != null)
+        {
+            attackButton.onClick.AddListener(Attack);
+        }
+    }
+
+    public void ShowMenu()
+    {
+        if (actionButton != null)
+        {
+            actionButton.gameObject.SetActive(true);
+        }
+
+        if (dropUpPanel != null)
+        {
+            dropUpPanel.SetActive(false);
+        }
+    }
+
+    public void HideMenu()
+    {
+        if (actionButton != null)
+        {
+            actionButton.gameObject.SetActive(false);
+        }
+
+        if (dropUpPanel != null)
+        {
+            dropUpPanel.SetActive(false);
+        }
+    }
+
+    private void ToggleDropUp()
+    {
+        if (dropUpPanel != null)
+        {
+            dropUpPanel.SetActive(!dropUpPanel.activeSelf);
+        }
+    }
+
+    private void Attack()
+    {
+        if (GameManager.instance.enemyTeam.Count > 0)
+        {
+            CharacterController enemy = GameManager.instance.enemyTeam[0];
+            int damage = Random.Range(10, 21);
+            enemy.TakeDamage(damage);
+            Debug.Log($"Enemy {enemy.name} takes {damage} damage. HP left: {enemy.hitPoints}");
+        }
+
+        if (dropUpPanel != null)
+        {
+            dropUpPanel.SetActive(false);
+        }
+    }
+}
