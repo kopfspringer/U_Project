8480678bc0fdf1fcaef2e06953416a528c59dc78
kopfspringer <74976028+kopfspringer@users.noreply.github.com>
2025-08-05 12:06:08 +0200

    Toggle magic buttons

--- Assets/Scripts/ActionMenu.cs
+++ Assets/Scripts/ActionMenu.cs
@@ -13,6 +13,9 @@ public class ActionMenu : MonoBehaviour
     [SerializeField] private Button magicButton;
     [SerializeField] private Button restButton;
 
+    private Button fireButton;
+    private Button rainButton;
+
     private void Awake()
     {
         instance = this;
@@ -90,12 +93,71 @@ public class ActionMenu : MonoBehaviour
 
     private void Magic()
     {
-        CompletePlayerAction();
+        if (fireButton == null || rainButton == null)
+        {
+            CreateMagicButtons();
+        }
+
+        bool show = !fireButton.gameObject.activeSelf;
+        fireButton.gameObject.SetActive(show);
+        rainButton.gameObject.SetActive(show);
     }
 
     private void Rest()
     {
         CompletePlayerAction();
+    }
+
+    private void CastFire()
+    {
+        CompletePlayerAction();
+    }
+
+    private void CastRain()
+    {
+        CompletePlayerAction();
+    }
+
+    private void CreateMagicButtons()
+    {
+        fireButton = CreateButton(dropUpPanel.transform, "Fire");
+        rainButton = CreateButton(dropUpPanel.transform, "Rain");
+
+        fireButton.gameObject.SetActive(false);
+        rainButton.gameObject.SetActive(false);
+
+        fireButton.onClick.AddListener(CastFire);
+        rainButton.onClick.AddListener(CastRain);
+    }
+
+    private Button CreateButton(Transform parent, string text)
+    {
+        GameObject buttonObj = new GameObject(text + "Button");
+        buttonObj.transform.SetParent(parent, false);
+
+        Image image = buttonObj.AddComponent<Image>();
+        image.color = Color.white;
+
+        Button button = buttonObj.AddComponent<Button>();
+
+        GameObject textObj = new GameObject("Text");
+        textObj.transform.SetParent(buttonObj.transform, false);
+        Text txt = textObj.AddComponent<Text>();
+        txt.font = Resources.GetBuiltinResource<Font>("LegacyRuntime.ttf");
+        txt.text = text;
+        txt.alignment = TextAnchor.MiddleCenter;
+        txt.color = Color.black;
+
+        RectTransform rect = buttonObj.GetComponent<RectTransform>();
+        rect.sizeDelta = new Vector2(160, 40);
+
+        RectTransform textRect = textObj.GetComponent<RectTransform>();
+        textRect.anchorMin = Vector2.zero;
+        textRect.anchorMax = Vector2.one;
+        textRect.offsetMin = Vector2.zero;
+        textRect.offsetMax = Vector2.zero;
+
+        return button;
     }
 
     private void CompletePlayerAction()
